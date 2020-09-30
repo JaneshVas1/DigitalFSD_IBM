87c01c1df6507719befb153cd67ee3eb3027463a
JaneshVasudeva <janesh_v@rediffmail.com>
2020-09-30 16:46:14 +0530

    OSchanges

--- practice/Nodejs/app.js
+++ practice/Nodejs/app.js
@@ -10,3 +10,14 @@ calc.subtract();
 calc.multiply();
 calc.divide();
 
+const os = require('os');
+let release=os.release();
+let platform=os.platform();
+let arc=os.arch();
+let hostName=os.hostname();
+let freeMem=os.freemem();
+let homeDir=os.homedir()
+let networkInterfaces=os.networkInterfaces();
+console.log(`${release} ${platform} ${arc} ${os.hostname()}`);
+
+
