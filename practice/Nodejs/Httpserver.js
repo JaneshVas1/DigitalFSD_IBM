d2ebfc39657293b2a96412d02da19b5c46c82b17
JaneshVasudeva <janesh_v@rediffmail.com>
2020-10-01 17:54:17 +0530

    nodemonhttpserver changes

--- practice/Nodejs/Httpserver.js
+++ practice/Nodejs/Httpserver.js
@@ -22,18 +22,20 @@ const server=http.createServer((req, res) => {
 
     res.setHeader('content-type','text/html');
     res.write('<html><head></head><body><h1> Welcome to node</h1></body></html>');
-    res.write('<h2> Your name is John Doe</h2>');
-    res.write('<h4> New world Looks Amazing</h4></body></html>');
+    res.write('<h2> Your name is John Doe Big Brother</h2>');
+    res.write('<h4> New world , Swim world , Looks Amazing</h4></body></html>');
     res.end();
 
-    })
+    }).listen(3000, ()=> {
+	console.log('node server started on port 3000');		
+	})
 
 
 //if we do not provide the port number by default the port is 8080
 
-server.listen(3000);
+//server.listen(3000);
 
-console.log('node server started on port 3000');
+
 
 
 
