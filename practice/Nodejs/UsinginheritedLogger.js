0c48fb441a70361a7242a285b3874d733ebf4deb
JaneshVasudeva <janesh_v@rediffmail.com>
2020-10-01 10:59:01 +0530

    Inheritancechanges

---
+++ practice/Nodejs/UsinginheritedLogger.js
@@ -0,0 +1,27 @@
+const EventEmitter = require('events');
+
+const Logger=require('./Inheritancelogger');
+
+const logger=new Logger();
+
+logger.log('calling log method from logger module');
+
+logger.log1('calling log1 method from logger module');
+
+logger.addListener('messageLogged',()=>{
+
+    console.log('Handled messageLogged Event');
+
+});
+
+logger.addListener('messageLogged',()=>
+
+{
+
+   console.log('Handled messageLogged1 Event');
+
+});
+
+
+
+logger.log('=======END=======');
