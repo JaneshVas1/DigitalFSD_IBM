8e27c3b053ccc66004e71286d42b88f0168d7755
JaneshVasudeva <janesh_v@rediffmail.com>
2020-10-07 12:55:09 +0530

    add styling

--- practice/Nodejs/routing/route/admin.js
+++ practice/Nodejs/routing/route/admin.js
@@ -1,18 +1,19 @@
 const express=require('express');
-
+const path=require('path');
 const router=express.Router();
-
+const rootDir=require('.././util/path');
 router.get('/add-product',(req, res) => {
 
-    res.send('<form action="/admin/product" method="post">' +
+   /* res.send('<form action="/admin/product" method="post">' +
 
         '<input type="text" name="title"><br/>' +
 
-        '<input type="submit" value="add product"></form>');
+        '<input type="submit" value="add product"></form>');*/
+        res.sendFile(path.join(rootDir, 'views','add-product.html'));
 
 });
 
-router.post('/product',(req, res) => {
+router.post('/add-product',(req, res) => {
 
     console.log(req.body);
 
