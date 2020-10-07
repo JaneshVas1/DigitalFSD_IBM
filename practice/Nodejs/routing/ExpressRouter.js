5b3982d2bb74e509bbb2162e2452a65bb26b5841
JaneshVasudeva <janesh_v@rediffmail.com>
2020-10-07 10:58:05 +0530

    error

--- practice/Nodejs/routing/ExpressRouter.js
+++ practice/Nodejs/routing/ExpressRouter.js
@@ -1,7 +1,7 @@
 const bodyParser=require('body-parser');
 
 const express=require('express');
-
+const path=require('path');
 const app = express();
 app.use(express.static('public'))
 
@@ -15,6 +15,11 @@ const shopRoutes=require('./route/shop');
 app.use('/admin',adminRoutes);
 
 app.use(shopRoutes);
+
+app.use('/', (req, res, next)=>{
+    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+});
+
 
 app.listen(3000,(() => {
 
