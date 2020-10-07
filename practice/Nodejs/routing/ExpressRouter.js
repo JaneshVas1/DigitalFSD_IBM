d3aba34b95b4e274966df11c9d48cdb1e2eeb06b
JaneshVasudeva <janesh_v@rediffmail.com>
2020-10-07 12:00:04 +0530

    helper changes

--- practice/Nodejs/routing/ExpressRouter.js
+++ practice/Nodejs/routing/ExpressRouter.js
@@ -11,13 +11,15 @@ app.use(bodyParser.urlencoded({extended: false}));
 const adminRoutes=require('./route/admin');
 
 const shopRoutes=require('./route/shop');
-
+const rootDir=require('./util/path');
 app.use('/admin',adminRoutes);
 
 app.use(shopRoutes);
 
 app.use('/', (req, res, next)=>{
-    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+	//path dirname
+    //res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+    res.status(404).sendFile(path.join(rootDir, 'views', '404.html'));
 });
 
 
@@ -26,3 +28,4 @@ app.listen(3000,(() => {
     console.log('listening on http://localhost:3000');
 
 }))
+
--- practice/Nodejs/routing/route/shop.js
+++ practice/Nodejs/routing/route/shop.js
@@ -2,10 +2,12 @@ const express=require('express');
 const path=require('path');
 const router=express.Router();
 
+const rootDir=require('.././util/path');
 router.get('/',(req, res) => {
 
     //res.send('<h1>Hello from Express!</h1>');
-    res.sendFile(path.join(__dirname, '../','views','shop.html'));
+    //res.sendFile(path.join(__dirname, '../','views','shop.html'));
+    res.sendFile(path.join(rootDir, 'views','shop.html'));
 
 });
 
