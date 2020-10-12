4260148499169c402cbb1a6955584291ce4b255c
JaneshVasudeva <janesh_v@rediffmail.com>
2020-10-12 17:55:34 +0530

    pug changes

--- practice/Nodejs/routing/ExpressRouter.js
+++ practice/Nodejs/routing/ExpressRouter.js
@@ -5,7 +5,9 @@ const path=require('path');
 const app = express();
 app.use(express.static('public'));
 
+app.set('view engine', 'pug');
 
+app.set('views','views');
 app.use(bodyParser.urlencoded({extended: false}));
 
 const adminRoutes=require('./route/admin');
@@ -19,7 +21,9 @@ app.use(shopRoutes);
 app.use('/', (req, res, next)=>{
 	//path dirname
     //res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
-    res.status(404).sendFile(path.join(rootDir, 'views', '404.html'));
+    // working changed to render 404 pug
+    //res.status(404).sendFile(path.join(rootDir, 'views', '404.html'));
+    res.render('404',{titleView:'error page'})
 });
 
 
