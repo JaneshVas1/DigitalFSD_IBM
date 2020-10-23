48879b70eca72e184e4fa884c779a0595e1876f9
JaneshVasudeva <janesh_v@rediffmail.com>
2020-10-23 12:59:29 +0530

    angular

---
+++ practice/angular/myfirstproject/src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  //templateUrl: './app.component.html',
+  templateUrl: './test.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'My First Project';
+  h1text= 'Registration Form';
+}
